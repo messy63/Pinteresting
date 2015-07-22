c59cf3975c202ca6c5ec2a92881cb5f281086d32
Jesse <messy63@gmail.com>
2015-07-22 14:05:06 -0400

    FIXED SYNTAX

--- Gemfile
+++ Gemfile
@@ -7,12 +7,14 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'turbolinks'
+gem 'jquery-turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0'
 gem "bootstrap-sass"
 gem 'devise'
 gem 'paperclip', '~> 4.2'
 gem 'aws-sdk' , '< 2.0'
+gem 'masonry-rails' , '~> 0.2.0'
 
 group :production do
 	gem 'rails_12factor'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,15 +78,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
    # Required for Heroku
    # Note set this to actual host
-  config.action_mailer.default_url_options = { host: 'https://jesse-moore.herokuapp.com/', port: 3000
+  config.action_mailer.default_url_options = { host: 'https://jesse-moore.herokuapp.com/', port: 3000}
 
     # Sets Paperclip to upload images to Amazon S3
   config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
-}
 end
